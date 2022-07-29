802b177db5d702995a715a9eeb443269b6fb9049
FocoSolution <paulo.freire@focosolution.com>
2022-07-29 16:27:04 -0300

    Primeiro commit em Primeiros passos em Spring Boot 3

---
+++ 05_FirstStepsInJavaWithSpringBoot/rest-with-spring-boot-and-java/src/test/java/br/com/focosolution/StartupTests.java
@@ -0,0 +1,13 @@
+package br.com.focosolution;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StartupTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
