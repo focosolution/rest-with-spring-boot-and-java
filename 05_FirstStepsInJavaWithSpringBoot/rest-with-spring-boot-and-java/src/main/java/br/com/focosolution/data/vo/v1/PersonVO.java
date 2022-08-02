a045d35360707b946ac6b8df23c53f964d4d1a86
FocoSolution <paulo.freire@focosolution.com>
2022-08-02 11:50:30 -0300

    Ajustes na finalização da seção 8

---
+++ 05_FirstStepsInJavaWithSpringBoot/rest-with-spring-boot-and-java/src/main/java/br/com/focosolution/data/vo/v1/PersonVO.java
@@ -0,0 +1,99 @@
+package br.com.focosolution.data.vo.v1;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+// Estas annotations servem apenas para informar ao JPA que trata-se de uma entidade / tabela
+@Entity
+@Table(name = "person")
+public class PersonVO implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private long id;
+	
+	@Column(name = "first_name", nullable = false, length = 80)
+	private String firstName; 
+	
+	@Column(name = "last_name")
+	private String lastName;
+	
+	@Column
+	private String address;
+	
+	@Column
+	private String gender;
+	
+	// JPA exige tal construtor	
+	public PersonVO() {}	
+	
+	@Override
+	public int hashCode() {
+		return Objects.hash(address, firstName, gender, id, lastName);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		PersonVO other = (PersonVO) obj;
+		return Objects.equals(address, other.address) && Objects.equals(firstName, other.firstName)
+				&& Objects.equals(gender, other.gender) && id == other.id && Objects.equals(lastName, other.lastName);
+	}
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public String getAddress() {
+		return address;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+	public String getGender() {
+		return gender;
+	}
+
+	public void setGender(String gender) {
+		this.gender = gender;
+	}
+
+	
+	
+}
